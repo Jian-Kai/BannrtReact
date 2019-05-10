ea622111e77af7f1257d12853912d212fdc15cd8
TonyKuo <tony14256@gmail.com>
2019-05-10 16:17:40 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import banner from './imgs/banner.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      default: {
+        // 設定一開始是否為開或合
+        openAtStart: true, // [boolean] true | false
+        // 設定啟動後是否要自動開或合，若設為false，就不要自勳開合；若為true是馬上自動開合；若為數字是幾毫秒之後開合
+        autoToggle: true, // [boolean|number] true | false | 3000
+        // 設定收合展開按鈕
+        button: {
+          closeText: '收合', // [string]
+          openText: '展開', // [string]
+          class: 'btn' // [string]
+        },
+        // 設定模組在各狀態時的class
+        bannerclass: {
+          closed: 'closed', // [string]
+          closing: 'closing', // [string]
+          opened: 'opened', // [string]
+          opening: 'opening' // [string]
+        },
+        // 是否要有transition效果
+        transition: true,
+        // 當有transition時，要執行的callback function
+        whenTransition: function () {
+          console.log('whenTransition');
+        }
+      },
+      status: null,
+      banner: null
+    };
+
+    this.clickEvent = this.clickEvent.bind(this);
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+  }
+
+  componentDidMount() {
+    const { openAtStart, bannerclass } = this.state.default;
+    this.setState({
+      status: openAtStart,
+      banner: (openAtStart) ? bannerclass.opened : bannerclass.closed
+    });
+
+    this.toggle();
+  }
+
+  toggle(){
+    const {autoToggle} = this.state.default;
+    console.log(autoToggle)
+  }
+
+  clickEvent() {
+    console.log("click")
+    const { status } = this.state;
+    //console.log(status);
+    (status) ? this.close() : this.open();
+    this.setState({ status: !status })
+  }
+
+  open() {
+    console.log("open")
+    const { bannerclass, transition, whenTransition } = this.state.default;
+
+    if (transition) {
+      //change status closed -> opening -> oopened
+      this.setState({ banner: bannerclass.opening })
+      const interval = setInterval(whenTransition,20);
+      setTimeout(function () {
+        this.setState({ banner: bannerclass.opened });
+        clearInterval(interval);
+      }.bind(this), 600);
+    } else
+      //change status closed -> oopened
+      this.setState({ banner: bannerclass.opened });
+
+  }
+
+  close() {
+    console.log("close")
+    const { bannerclass, transition, whenTransition } = this.state.default;
+
+    if (transition) {
+      //change status oopened -> closing -> closed
+      this.setState({ banner: bannerclass.closing });
+      const interval = setInterval(whenTransition,20);
+      setTimeout(function () {
+        this.setState({ banner: bannerclass.closed });
+        clearInterval(interval);
+      }.bind(this), 600);
+    }
+    else
+      //change status oopened -> closing -> closed
+      this.setState({ banner: bannerclass.closed });
+
+  }
+
+  render() {
+    const { button } = this.state.default;
+
+    const status = this.state.banner;
+    const text = (this.state.status) ? button.closeText : button.openText;
+    return (
+      <div className={"banner " + status}>
+        <a className="wrap" href="#" >
+          <img className="img" src={banner} title="輸入廣告促銷說明文字" alt="輸入廣告促銷說明文字" />
+          <button className={button.class} onClick={this.clickEvent}>{text}</button>
         </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
